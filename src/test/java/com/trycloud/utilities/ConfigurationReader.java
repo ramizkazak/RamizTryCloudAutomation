eb86a9db2bcfe58c79cf01dfd63d87e73ea8ab8b
ramizkazak <ramizkazak@gmail.com>
2021-01-30 23:52:28 -0600

    Create new project for extra practice for new project call TryCloud

---
+++ src/test/java/com/trycloud/utilities/ConfigurationReader.java
@@ -0,0 +1,6 @@
+package com.trycloud.utilities;
+
+public class ConfigurationReader {
+    //In this class we will implement the repeated steps of reading
+    // from configuration.properties file
+}
